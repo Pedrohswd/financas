28121d35cd018e175b5c4772840e5d6f18d7e6ec
Pedrohswd <pedrohsfwd@gmail.com>
2024-10-04 16:39:57 -0300

    ajuste de tabela

--- src/main/java/com/gestao/financas/model/Pessoa.java
+++ src/main/java/com/gestao/financas/model/Pessoa.java
@@ -12,7 +12,7 @@ public class Pessoa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100, unique = true)
     private String nome;
 
     @Column(nullable = false, unique = true, length = 14)
